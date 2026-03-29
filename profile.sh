61099b1e9ad23fdd5763c6c3a6dc69a834c9253e
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2026-03-28 22:34:02 -0400

    fix profile script

--- profile.sh
+++ profile.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-cargo b --release --bin parse_hits_100 && samply record ./target/release/parse_hits_100
+cargo b --release --bin parse_hits && samply record ./target/release/parse_hits "$@"
